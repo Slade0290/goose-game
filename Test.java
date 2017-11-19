7259ffe6da4a2e223bb7bd18831d9146d350d5e9
Matthieu <Matthieu@Matt.home>
2017-11-19 17:04:38 +0100

    super test

--- Test.java
+++ Test.java
@@ -4,10 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		Pion p = new Pion();
-		System.out.println("BORDEL DE MERDE");
-		System.out.println("BORDEL DE MERDE2");
-		System.out.println("BORDEL DE MERDE3");
-		System.out.println("BORDEL DE MERDE3");
 		System.out.println("BORDEL DE MERDE3");
 		
 	}
