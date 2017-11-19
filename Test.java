09912312c17ee574499b6eba28a3091c1b9b81dd
Matthieu <Matthieu@Matt.home>
2017-11-19 16:58:05 +0100

    lalalala

--- Test.java
+++ Test.java
@@ -4,6 +4,9 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		Pion p = new Pion();
+		System.out.println("BORDEL DE MERDE");
+		System.out.println("BORDEL DE MERDE2");
+		System.out.println("BORDEL DE MERDE3");
 		
 	}
 
