48cc904e581146d752dcb9219a6f03b39f40343b
Slade0290 <ly.david@live.fr>
2017-11-24 17:11:33 +0100

    Create CaseQuestion.java

---
+++ CaseQuestion.java
@@ -0,0 +1,12 @@
+public class CaseQuestion {
+
+  private Question question;
+  
+  public CaseQuestion(){
+    this.question = questionRandom();
+  }
+  
+  private Question questionRandom(){
+    return ; //question aléa de la banque de question
+  }
+}
