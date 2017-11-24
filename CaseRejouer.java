ab6c0e653b352853de19b95d924a5e7c00d59f6e
Slade0290 <ly.david@live.fr>
2017-11-24 17:30:31 +0100

    Update CaseRejouer.java

--- CaseRejouer.java
+++ CaseRejouer.java
@@ -1,15 +1,9 @@
 public class CaseRejouer extends Case{
 
-
-
-
   public CaseRejouer(Pion p){
-  super(p);
+    super(p);
+  }
   
-  
-  
-  public void rejouer()
-  
-  
-  
+  public void rejouer(){ 
+    //appelle à nouveau la fonction jouer, relance le de etc..
   }
