17dbe7b22d6e4a05a4e4e2bf6bdfd30e9f1fb33a
Slade0290 <ly.david@live.fr>
2017-11-24 17:48:43 +0100

    Update IAOie.java

--- IAOie.java
+++ IAOie.java
@@ -6,5 +6,9 @@ public IAOie(boolean d){
   this.difficulte = d;
 }
   
+public int reponse(){
+  //va chercher dans la banque de question, une réponse comprise entre tel ou tel tranche de réponse en fonction de la difficulté
+}
+  
   
 }
