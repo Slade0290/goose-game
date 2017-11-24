a6c981f1fcd87cbb5259592dd5d7d3d5d0caa907
Slade0290 <ly.david@live.fr>
2017-11-24 17:21:56 +0100

    Update De.java

--- De.java
+++ De.java
@@ -1,12 +1,18 @@
+import java.util.Random;
+
 public class De{
-  private int nbFaces;
   
-  public De(int n){
-    this.nbFaces = n;
-    }
+  private int nbFaces;
+  public static Random = new Random();
+  
+  public De(){
+    this.nbFaces = 6;
+  }
+    
+  public int lancerDeDe(){
+    return rand.nextInt(nbFaces);
+  }
     
     
-    
-    
-    }
+}
     
