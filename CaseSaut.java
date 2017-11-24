6d29ca0328dd7f0617456de8d8c58b7c73e24e9f
Slade0290 <ly.david@live.fr>
2017-11-24 17:13:59 +0100

    Create CaseSaut.java

---
+++ CaseSaut.java
@@ -0,0 +1,9 @@
+public class CaseSaut {
+  
+  private int destination;
+  
+  public CaseSaut(){
+    this.destination = rand.nextInt(à partir de la position de la case jusqu'a taille du tableau - position actuelle); 
+  }
+  
+}
